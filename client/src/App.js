2ca3defe733a4fb8bfd824f970515a4e177e54dd
manueloglez <mang.95@gmail.com>
2021-06-30 23:38:20 -0700

    login implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,44 @@
+import React, {useState, useEffect} from 'react'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import StudentPage from "./components/StudentPage";
 import Navbar from "./components/Navbar";
 import SignUp from "./components/login/SignUp";
 import SignIn from "./components/login/SignIn";
+import { User, Session } from './requests';
 import './App.css'
 
 const App = () => {
+  const [state, setState] = useState({user: null})
+
+  const getCurrentUser = () => {
+    return User.current().then(user => {
+      if (user?.id) { 
+        setState(state => {
+          console.log(user)
+          return { user }
+        })
+      }
+    })
+  }
+
+  const destroySession = () => {
+    Session.destroy().then((res) => {
+      setState((state) => {
+        return { user: null };
+      });
+    });
+  }
+
+  useEffect(() => {
+    getCurrentUser()
+  }, [])
+
   return <div className="App"> 
   <BrowserRouter>
-    <Navbar />
+    <Navbar currentUser={state.user} destroySession={destroySession}/>
     <Switch>
       <Route exact path="/students" component={StudentPage}/>
-    </Switch>
-    <Switch>
-      <Route exact path="/SignIn" component={SignIn}/>
-    </Switch>
-    <Switch>
+      <Route exact path='/SignIn' render={(routeProps)=><SignIn {...routeProps} onSignIn={getCurrentUser}/>} />
       <Route exact path="/SignUp" component={SignUp}/>
     </Switch>
   </BrowserRouter>
