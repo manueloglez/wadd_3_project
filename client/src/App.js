8749b945efedb1972684c28a0a32a31706d3c2d4
Anjali VAghela <anjuvaghela007@gmail.com>
2021-06-30 21:30:54 -0700

    Sign In and Sign Up

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import StudentPage from "./components/StudentPage";
 import Navbar from "./components/Navbar";
+import SignUp from "./components/login/SignUp";
+import SignIn from "./components/login/SignIn";
 import './App.css'
 
 const App = () => {
@@ -9,6 +11,12 @@ const App = () => {
     <Navbar />
     <Switch>
       <Route exact path="/student" component={StudentPage}/>
+    </Switch>
+    <Switch>
+      <Route exact path="/SignIn" component={SignIn}/>
+    </Switch>
+    <Switch>
+      <Route exact path="/SignUp" component={SignUp}/>
     </Switch>
   </BrowserRouter>
   </div>
