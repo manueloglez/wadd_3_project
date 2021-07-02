de40c12a2dd23425d2c07bf32f310d16f11b7ccf
Sree <vjsreekanth@gmail.com>
2021-07-01 19:18:51 -0700

    added index to enrollments

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do #👈🏻 we can set default response format of the block
     namespace :v1 do
       resources :courses, only: [:create, :destroy, :update, :show, :index] do
-        resources :enrollments, shallow: :true, only: [:create, :destroy, :update]
+   
+        resources :enrollments, shallow: :true, only: [:create, :destroy, :update, :index]
       end
       resource :session, only: [:create, :destroy]
       get("/current_user", to: "sessions#get_current_user")
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_012123) do
+ActiveRecord::Schema.define(version: 2021_07_02_014747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,18 @@ ActiveRecord::Schema.define(version: 2021_07_02_012123) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "facility_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["facility_id"], name: "index_reservations_on_facility_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -57,4 +69,6 @@ ActiveRecord::Schema.define(version: 2021_07_02_012123) do
   add_foreign_key "courses", "users"
   add_foreign_key "enrollments", "courses"
   add_foreign_key "enrollments", "users"
+  add_foreign_key "reservations", "facilities"
+  add_foreign_key "reservations", "users"
 end
