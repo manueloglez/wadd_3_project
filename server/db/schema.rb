5da57ae4edfd52472aa6fdf1d133fdcdbb595b8c
Sree <vjsreekanth@gmail.com>
2021-07-01 18:26:58 -0700

    created facility model

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_192429) do
+ActiveRecord::Schema.define(version: 2021_07_02_012123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2021_07_01_192429) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["course_id"], name: "index_enrollments_on_course_id"
     t.index ["user_id"], name: "index_enrollments_on_user_id"
+  end
+
+  create_table "facilities", force: :cascade do |t|
+    t.string "name"
+    t.text "location"
+    t.boolean "is_available", default: true
+    t.string "features", default: [], array: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
